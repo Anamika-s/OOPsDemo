5ef1320f4a36b5f82a547e46f49a3c391f6527a2
Anamika-s <anamika.sawhney22@gmail.com>
2023-07-21 15:26:29 +0530

    Add project files.

---
+++ OOPsDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace OOPsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student1 = new Student();
+            //student1.GetDetails();
+
+            //student1.DisplayDetails();
+            //Student student2 = new Student();
+            //student2.GetDetails();
+
+            //student2.DisplayDetails();
+            //Student student3 = new Student();
+            //student3.GetDetails();
+
+            //student3.DisplayDetails();
+
+            //// WAP toi take 5 no;s's and addt them
+            //int x, y, z, a, b;
+            //Console.WriteLine("Enter No1");
+            //x = 
+            // We use Array
+            // Array of objects
+            Student[] students = new Student[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Enter Details of {i + 1} Student");
+                students[i] = new Student();
+                students[i].GetDetails();
+            }
+            Console.WriteLine("Company is " + Student.company);
+            Student.DisplayBatch();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Details of {i + 1} Student are");
+                students[i].DisplayDetails();
+            }
+        }
+    }
+}
+
+// CLass > USer Defined Type
+// Blueprint based on which multiple objects of same type cud be created
+// Data Type > 1. What we can store , What functions can be performed 3. Size in bytes
+// int > 12,34,45, + - min max avg , 32 bits// 
+// Data Types > 1. Inbuilt / primitive 2. UD Types
+// Inbuilt > int char float bool string 
+// UD > class struct enum
+// class > UDType
+// class Student
+//{
+//    int rn;
+//    string name;
+//    void Get() { }
+//            void DIsplay() {  }
+//}
+
+// Int 
+// Student 
+
+
+//int x;
+//Student obj;
+// Object > instanace or variable of a class
---
+++ OOPsDemo/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo
+{
+    internal class  Student
+    {
+        int id;
+        string name;
+
+
+
+        
+        static string batchName="B001111111111111111111111111111111";
+        public const string company="CGI";
+        
+        public void GetDetails()
+        {
+            Console.WriteLine("Enter I222232323232323d");
+            id = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Id is {id}");
+            Console.WriteLine($"Name is {name}");
+             
+
+            }
+        public static void DisplayBatch()
+        {
+            batchName = "B002";
+            Console.WriteLine($"batchName is {batchName}");
+             
+
+        }
+    }
+}
